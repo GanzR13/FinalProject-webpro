30027ed09d9a1e33affa270642ae2837a1f9b093
Buzank <buzankkun@gmail.com>
2023-07-13 08:43:08 +0700

    Revert "Json Implementation on Gallery and new About use"

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Gambar from '../Destinasi.json';
 
 const Activities = () => {
     return (
         <div className="max-w-6xl m-auto w-full md:flex mt-[-100px]">
-            {Gambar.slice(0, 3).map((Destinasi, index) => (
+          {Gambar.slice(0, 3).map((Destinasi, index) => (
             <div className="relative p-4 flex-1" key={index}>
               <div className='relative'>
                 <h3 className="absolute bottom-0 left-0 p-4 text-white text-2xl font-bold">
@@ -17,7 +16,9 @@ const Activities = () => {
                     alt=""  
                   />
                 </div>
+              </div>
             </div>
+          ))}
         </div>
     );
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react'
 
 const About = () => {
 	return (
-		<div className="">
+		<div className=''>
 			<div className="w-full h-96 bg-blue-300"></div>
 			<div className="m-auto items-center">
 				<div className="absolute top-[20%] w-full md:-[50%] h-full flex flex-col text-gray-700 p-4 items-center">
@@ -29,5 +29,22 @@ const About = () => {
 		</div>
 	);
 };
+  return (
+    <div className=''>
+      <div className="w-full h-96 bg-blue-300">
+      </div>
+      <div className='m-auto items-center'>
+          <div className='absolute top-[20%] w-full md:-[50%] h-full flex flex-col text-gray-700 p-4 items-center'>
+            <h1 className="font-bold text-4xl">About Us</h1>
+            <p className="text-xl py-4 italic">Duo of Rizky</p>
+          </div>
+      </div>
+      <div className='max-w-[1140px] m-auto w-full p-4 py-16'>
+      </div>
+    </div>
+  )
+}
 
 export default About
+
+export default About;
